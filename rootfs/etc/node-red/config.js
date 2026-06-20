aa94fd75f3dc03c068005da1ec35cf1b52ebc544
IevgenVaskivskyi <52214026+IevgenVaskivskyi@users.noreply.github.com>
2026-06-20 22:30:52 +0300

    Update config.js

--- rootfs/etc/node-red/config.js
+++ rootfs/etc/node-red/config.js
@@ -41,27 +41,27 @@ config.contextStorage = {
     }
   }
 };
+//
+//config.adminAuth = {
+//  type: "credentials",
+//  users: [
+//    {
+//      username: "pi",
+//      password: "$2a$08$uGLFsGppdWnckZpomdNQveucw.zh8bkSWDO0Gnzj4Z0asqj91KKge",
+//      permissions: "*"
+//    }
+//  ]
+//};
 
-config.adminAuth = {
-  type: "credentials",
-  users: [
-    {
-      username: "pi",
-      password: "$2a$08$uGLFsGppdWnckZpomdNQveucw.zh8bkSWDO0Gnzj4Z0asqj91KKge",
-      permissions: "*"
-    }
-  ]
-};
+//config.httpNodeAuth = {
+//  user: "pi",
+//  pass: "$2a$08$uGLFsGppdWnckZpomdNQveucw.zh8bkSWDO0Gnzj4Z0asqj91KKge"
+//};
 
-config.httpNodeAuth = {
-  user: "pi",
-  pass: "$2a$08$uGLFsGppdWnckZpomdNQveucw.zh8bkSWDO0Gnzj4Z0asqj91KKge"
-};
-
-config.httpStaticAuth = {
-  user: "pi",
-  pass: "$2a$08$uGLFsGppdWnckZpomdNQveucw.zh8bkSWDO0Gnzj4Z0asqj91KKge"
-};
+//config.httpStaticAuth = {
+//  user: "pi",
+//  pass: "$2a$08$uGLFsGppdWnckZpomdNQveucw.zh8bkSWDO0Gnzj4Z0asqj91KKge"
+//};
 
 if (options.log_level) {
   config.logging.console.level = options.log_level.toLowerCase();
